6c732def79d5475710405715373df8297b5ce837
lichen777 <lchen139@gmail.com>
2017-11-28 15:31:38 -0600

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,46 +10,55 @@ const trivia = {
 			question: "The city of Maracaibo and its lake are in what country?",
 			options: ["Morocco", "Spain", "Venezuela", "Colombia"],
 			correctAnswer: 2,
+			picture: "assets/images/Venezuela.png"
 		},
 		{
 			question: "What range of hills runs 50 miles from the Worcestershire border through Gloucestershire to the Avon?",
 			options: ["Caucasus Mountains", "Pennines", "Carpathian Mountains", "Cotswolds"],
 			correctAnswer: 3,
+			picture: "assets/images/Cotswolds.png"
 		},
 		{
 			question: "Which of these states of the USA extends the furthest north?",
 			options: ["Vermont", "Texas", "New Mexico", "Florida"],
 			correctAnswer: 0,
+			picture: "assets/images/Vermont.png"
 		},
 		{
 			question: "Which of these places is furthest from Perth, Australia?",
 			options: ["Beijing, China", "Bangkok, Thailand", "Tokyo, Japan", "Mumbai, India"],
 			correctAnswer: 0,
+			picture: "assets/images/Beijing.png"
 		},
 		{
 			question: "What is the deepest known part of the world's oceans, and the deepest location on the surface of the Earth's crust?",
 			options: ["The Kate Gully", "The Josephine Ditch", "The Mariana Trench", "The Emma Furrow"],
 			correctAnswer: 2,
+			picture: "assets/images/Mariana.png"
 		},
 		{
 			question: "The north of which country falls within the Sahel?",
 			options: ["Burkina Faso", "South Africa", "Libya", "Zimbabwe"],
 			correctAnswer: 0,
+			picture: "assets/images/BurkinaFaso.png"
 		},
 		{
 			question: "Where is the Gulf of Ob?",
 			options: ["Barents Sea", "Sea of Okhotsk", "Lake Balkhash", "Kara Sea"],
 			correctAnswer: 3,
+			picture: "assets/images/KaraSea.png"
 		},
 	],
 	timer (time) {
 	  var remainingTime = time;
+	  $("#time").text("Time Remaining: " + remainingTime + " Seconds");
 	  if (!trivia.clockRunning) {
         trivia.intervalId = setInterval(function () {
-          $("#time").text("Time Remaining: " + remainingTime + " Seconds");
           remainingTime --;
+          $("#time").text("Time Remaining: " + remainingTime + " Seconds");
+          
           //console.log(remainingTime);
-          if(remainingTime < 0) {
+          if(remainingTime == 0) {
       	    clearInterval(trivia.intervalId);
       	    trivia.clockRunning = false;
       	    return trivia.timeout();
@@ -59,12 +68,14 @@ const trivia = {
       }
 	},
 	start () {
+	  $("#display").empty();
+	  $("#question").show();
 	  $("#question").html("<div>" + trivia.QA[trivia.questionNum]["question"] + "</div>");
 	  var options = [...trivia.QA[trivia.questionNum]["options"]];
 	  var sequence = trivia.sequenceGenerate (options.length);
 
 	  for (var i = 0; i < options.length; i++) {
-	  	var addLine = $("<div>" + options[sequence[i]] + "</div>").addClass("choice").attr("data-option", sequence[i]);
+	  	var addLine = $("<div>" + options[sequence[i]] + "</div>").addClass("choice w-75").attr("data-option", sequence[i]);
 	    $("#question").append(addLine); 
 	  }
 	  trivia.timer(10); //set clock time.
@@ -84,31 +95,49 @@ const trivia = {
 	},
 
 	timeout () {
-	  $("#time").text("out of time!");
+	  $("#display").html("<div>Out Of Time! Correct answer is <b>" + trivia.QA[trivia.questionNum]["options"][trivia.QA[trivia.questionNum]["correctAnswer"]] + "</b>.</div>");
+	  $("#display").append("<img src=" + trivia.QA[trivia.questionNum]["picture"] + "width = '300px'>");
 	  trivia.unanswered++;
+	  $("#time").empty();
 	  $("#question").hide();
 	  trivia.goNext();
 	},
 	win () {
-	  $("#display").text("You are right!");
+	  $("#display").html("<div>Correct! You are awesome!</div>");
+	  $("#display").append("<img src='assets/images/correct.gif' width = '300px' >");
 	  trivia.correctCount++;
+	  $("#time").empty();
 	  $("#question").hide();
 	  trivia.goNext();
 	},
 	lose () {
-	  $("#display").text("You are wrong!");
+	  $("#display").html("<div>Wrong! Correct answer is <b>" + trivia.QA[trivia.questionNum]["options"][trivia.QA[trivia.questionNum]["correctAnswer"]] + "</b>.</div>");
+	  $("#display").append("<img src=" + trivia.QA[trivia.questionNum]["picture"] + " width = '300px'>");
 	  trivia.incorrectCount++;
+	  $("#time").empty();
 	  $("#question").hide();
 	  trivia.goNext();
 	},
 	goNext () { 
 	  trivia.questionNum++;
-
 	  setTimeout(function () {
-	  	$("#display").empty();
-	  	$("#time").empty();
-        $("#question").show();
-  		trivia.start();
+	    if (trivia.questionNum === trivia.QA.length) {
+	  	  $("#time").empty();
+	  	  $("#display").html("<div>All done!</div>");
+	  	  $("#display").append("<div>Correct: " + trivia.correctCount + "</div>");
+	  	  $("#display").append("<div>Incorrect: " + trivia.incorrectCount + "</div>");
+	  	  $("#display").append("<div>Unanswered: " + trivia.unanswered + "</div>");
+	  	  $("#start").show();
+	  	  trivia.questionNum = 0;
+	  	  trivia.correctCount = 0;
+	  	  trivia.incorrectCount = 0;
+	  	  trivia.unanswered = 0;
+	    } else {
+	  	  $("#display").empty();
+	  	  $("#time").empty();
+          $("#question").show();
+  		  trivia.start();
+  		}
   	  }, 3000);
 	},
 
@@ -124,7 +153,7 @@ const trivia = {
 }
 
 $(document).ready(function () {
-  $("#start").click(function () {
+  $("button").click(function () {
     $("#start").hide();
   	trivia.start();
   });
